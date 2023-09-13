e7efd01be3055cf52eeae0f18e07d9ecd39c0dc2
jerryp330 <110803469+jerryp330@users.noreply.github.com>
2023-09-13 16:13:53 -0600

    hello world w

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    render_template('index.html')
+    return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run(debug=True)
